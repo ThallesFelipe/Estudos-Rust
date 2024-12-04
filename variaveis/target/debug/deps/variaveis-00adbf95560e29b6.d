8c0e57d4950762dcb015cbb3ee62f12cb13d2ac4
Thalles F. <thalles.almeida@aluno.ufop.edu.br>
2024-12-04 20:56:13 -0300

    🦀

---
+++ variaveis/target/debug/deps/variaveis-00adbf95560e29b6.d
@@ -0,0 +1,5 @@
+C:\Users\thall\Documents\Estudos Rust\variaveis\target\debug\deps\libvariaveis-00adbf95560e29b6.rmeta: src/main.rs
+
+C:\Users\thall\Documents\Estudos Rust\variaveis\target\debug\deps\variaveis-00adbf95560e29b6.d: src/main.rs
+
+src/main.rs:
---
+++ variaveis/target/debug/deps/variaveis-276dcec412037478.d
@@ -0,0 +1,5 @@
+C:\Users\thall\Documents\Estudos Rust\variaveis\target\debug\deps\libvariaveis-276dcec412037478.rmeta: src/main.rs
+
+C:\Users\thall\Documents\Estudos Rust\variaveis\target\debug\deps\variaveis-276dcec412037478.d: src/main.rs
+
+src/main.rs:
