243a7997288d70765af465b3240ba32e9e11947f
Thalles F. <thalles.almeida@aluno.ufop.edu.br>
2024-12-04 20:34:03 -0300

    Olá, mãe! Estou aprendendo Rust! 🦀

---
+++ ola_mae/target/debug/deps/ola_mae-ae17fe78d6ff1a45.d
@@ -0,0 +1,5 @@
+C:\Users\thall\Documents\Estudos Rust\ola_mae\target\debug\deps\libola_mae-ae17fe78d6ff1a45.rmeta: src/main.rs
+
+C:\Users\thall\Documents\Estudos Rust\ola_mae\target\debug\deps\ola_mae-ae17fe78d6ff1a45.d: src/main.rs
+
+src/main.rs:
---
+++ ola_mae/target/debug/deps/ola_mae-e1be484144257d33.d
@@ -0,0 +1,5 @@
+C:\Users\thall\Documents\Estudos Rust\ola_mae\target\debug\deps\libola_mae-e1be484144257d33.rmeta: src/main.rs
+
+C:\Users\thall\Documents\Estudos Rust\ola_mae\target\debug\deps\ola_mae-e1be484144257d33.d: src/main.rs
+
+src/main.rs:
