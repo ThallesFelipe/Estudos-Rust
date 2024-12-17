945200f2eeedad6547c9f6b150dd432f3287746b
Thalles F. <thalles.almeida@aluno.ufop.edu.br>
2024-12-16 22:47:26 -0300

    Tirando warnins e printando. 🦀

--- exercicios_rust/src/exercicios/shadowing_execs.rs
+++ exercicios_rust/src/exercicios/shadowing_execs.rs
@@ -1,12 +1,19 @@
 fn main() {
     let mut x: i32 = 1;
+    println!("x: {x}");
+    
     x = 7;
+    println!("x: {x}");
 
     let mut x: i32 = x;
     x += 3; // x == 10
+    println!("x: {x}");
 
-    let y = 4;
-    let y = "Eu também posso ser vinculada à texto!";
+    let y: i32 = 4;
+    println!("y: {y}");
+
+    let y: &str = "Eu também posso ser vinculada à texto!";
+    println!("y: {y}");
 
     println!("Sucesso!");
 }
